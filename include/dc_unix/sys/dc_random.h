509630cac8adb2667adab18fba4884ccd7aa5b69
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2022-02-26 09:59:16 -0800

    added getentropy and getrandom

---
+++ include/dc_unix/sys/dc_random.h
@@ -0,0 +1,31 @@
+#ifndef LIBDC_UNIX_SYS_RANDOM_H
+#define LIBDC_UNIX_SYS_RANDOM_H
+
+/*
+ * Copyright 2022-2022 D'Arcy Smith.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dc_posix/dc_posix_env.h>
+#include <sys/types.h>
+
+/**
+ *
+ * @param env
+ * @param errvoid
+ * @return
+ */
+ssize_t dc_getrandom(const struct dc_posix_env *env, struct dc_error *err, void *buf, size_t buflen, unsigned int flags);
+
+#endif // LIBDC_UNIX_SYS_RANDOM_H

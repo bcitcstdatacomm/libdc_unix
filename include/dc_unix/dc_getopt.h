faea9c344fe351f167ba1c2e4399fcd47e8e8651
programming101dev <113087924+programming101dev@users.noreply.github.com>
2022-11-03 21:28:06 -0700

    some changes

---
+++ include/dc_unix/dc_getopt.h
@@ -0,0 +1,16 @@
+#ifndef DC_UNIX_DC_GETOPT_H
+#define DC_UNIX_DC_GETOPT_H
+
+
+#include <dc_env/env.h>
+#include <getopt.h>
+
+
+int dc_getopt_long(const struct dc_env *env, struct dc_error *err, int argc, char *const argv[],
+                   const char *optstring, const struct option *longopts, int *longindex);
+
+int dc_getopt_long_only(const struct dc_env *env, struct dc_error *err, int argc, char *const argv[],
+                        const char *optstring, const struct option *longopts, int *longindex);
+
+
+#endif //DC_UNIX_DC_GETOPT_H
